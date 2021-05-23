b9c3aa68323d467e07e733b1a6dadeca8b67a7d6
watanuki <watanukipublic@gmail.com>
2021-05-23 16:12:29 +0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
 import Tsuyobaha from './components/Tsuyobaha'
 import CalcSpeed from './components/CalcSpeed';
+import './App.css'
 
 export default class App extends Component {
     render() {
         return (
-            <div>
+            <div className="app">
                 <Tsuyobaha/>
                 <CalcSpeed/>
             </div>
